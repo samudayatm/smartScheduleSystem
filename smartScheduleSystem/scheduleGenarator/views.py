0a36f7c301869c00300b7b4f4aa216d66d209d3a
samudayatm <samudayatm@gmail.com>
2019-10-31 21:40:22 +0530

    initial commit

--- smartScheduleSystem/scheduleGenarator/views.py
+++ smartScheduleSystem/scheduleGenarator/views.py
@@ -10,7 +10,6 @@ BASE = os.path.dirname(os.path.abspath(__file__))
 CATEGORIES = ["Fat", "Thin"]
 
 
-
 def prepare(filepath):
     IMG_SIZE = 100  # 50 in txt-based
     img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
@@ -35,6 +34,25 @@ def process(request):
     model = tf.keras.models.load_model(os.path.join(BASE, "body-CNN.model"))
     template = loader.get_template('scheduleGenarator\schedule.html')
     imagePath = request.GET['imagePath']
+    name = request.GET['name']
+    # genderF = request.GET['genderF']
+    # genderM = request.GET['genderM']
+    height = request.GET['height']
+    weight = request.GET['weight']
+    bodyFat = request.GET['bodyFat']
+    pressure = request.GET['pressure']
+
+    print(name)
+    print(height)
+    print(weight)
+    print(bodyFat)
+    print(pressure)
+
+    if (int(pressure) > 100):
+        pressureCondition = 'HIGH'
+    else:
+        pressureCondition = 'LOW'
+
     data_folder = "D:\\project1\\ProjectFinal\\test_images\\"
     file_to_open = data_folder + imagePath
     print(file_to_open)
@@ -42,7 +60,63 @@ def process(request):
     # prediction = model.predict([prepare(data_folder + '11.jpg')])
     res = CATEGORIES[int(prediction[0][0])]
 
-    context = {
-        'status': res,
-    }
+    if (res == 'Thin'):
+        if (int(weight) < 45):
+            context = {
+                'bodyType': res,
+                'name': name,
+                'pressureCondition': pressureCondition,
+                'running': "10 min",
+                'pullover': "4reps 5 sets",
+                'front': "6reps 3 sets",
+                'back': "6reps 3 sets",
+                'bench': "6reps 3 sets",
+                'curl': "6reps 3 sets",
+                'triceps': "6reps 3 sets",
+                'situps': "20reps 3 set",
+            }
+        else:
+            context = {
+                'bodyType': res,
+                'name': name,
+                'pressureCondition': pressureCondition,
+                'running': "15 min",
+                'pullover': "6reps 4 sets",
+                'front': "6reps 4 sets",
+                'back': "6reps 4 sets",
+                'bench': "8reps 4 sets",
+                'curl': "8reps 3 sets",
+                'triceps': "6reps 3 sets",
+                'sitrunseups': "20reps 3 set",
+            }
+    else:
+        if(int(weight)>100):
+            context = {
+                'bodyType': res,
+                'name': name,
+                'pressureCondition': pressureCondition,
+                'running': "60 min",
+                'pullover': "16rep 4 sets",
+                'front': "12reps 4 sets",
+                'back': "12reps 4 sets",
+                'bench': "10reps 4 sets",
+                'curl': "10reps 4 sets",
+                'triceps': "12reps 3 sets",
+                'situps': "40reps 4 set",
+            }
+        else:
+            context = {
+                'bodyType': res,
+                'name': name,
+                'pressureCondition': pressureCondition,
+                'running': "45 min",
+                'pullover': "12reps 4 sets",
+                'front': "12reps 4 sets",
+                'back': "12reps 4 sets",
+                'bench': "10reps 4 sets",
+                'curl': "8reps 4 sets",
+                'triceps': "12reps 3 sets",
+                'situps': "30reps 4 set",
+            }
+
     return HttpResponse(template.render(context, request))
